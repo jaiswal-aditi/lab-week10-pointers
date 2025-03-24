bd1fb96ac437f708ae1c09d0875bcfc94283a771
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-03-23 16:05:39 -1000

    Update and rename matrix_operations.c to array_w_pointers.c

---
+++ array_w_pointers.c
@@ -0,0 +1,13 @@
+/*
+Write a C program that:
+1. allows the user to provide the number of elements in an array.
+2. allows the user to provide the elements of the array and store them in a dynamically allocated array.
+3. Uses pointers to:
+  o Print the array in the order entered.
+  o Print the array in reverse order.
+  o Sort the array in ascending order.
+  o Swap n elements of the array in cyclic order
+  - And displays the outputs for each of these conditions.
+
+** See PDF for sample input/output and detailed requirement.
+*/
